cd256673872b26806f48b527aea7dc9e6f50bc52
wbcomdev <pallavi@wbcomdesigns.com>
2020-09-29 11:43:47 +0530

    change file locations

--- bp-modify-member-directory.php
+++ bp-modify-member-directory.php
@@ -36,7 +36,7 @@ if ( ! defined( 'BPMMD_PLUGIN_URL' ) ) {
 	define( 'BPMMD_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 }
 if ( ! defined( 'BPMMD_PLUGIN_PATH' ) ) {
-	define( 'BPMMD_PLUGIN_PATH', plugin_dir_url( __FILE__ ) );
+	define( 'BPMMD_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
 }
 
 /**
